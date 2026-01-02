1b7e2bf67af0f3d09907728d8b7a04545589735e
zs-zhuang <z.summer.zhuang@gmail.com>
2026-01-02 13:04:02 -0800

    Full Clean Reset

---
+++ scdb_code_map/lawType_code.py
@@ -0,0 +1,12 @@
+# Auto-generated mapping: LAW_TYPE_MAP
+# Do not edit by hand
+
+LAW_TYPE_MAP = {1: 'Constitution',
+ 2: 'Constitutional Amendment',
+ 3: 'Federal Statute',
+ 4: 'Court Rules',
+ 5: 'Other (e.g., Treaties, Executive Orders)',
+ 6: 'State Statute',
+ 7: 'State Constitutional Provision',
+ 8: 'State Court Rule',
+ 9: 'No Legal Provision (e.g., Common Law)'}
---
+++ scdb_code_map/petitioner_code.py
@@ -0,0 +1,13 @@
+# Auto-generated mapping: PETITIONER_MAP
+# Do not edit by hand
+
+PETITIONER_MAP = {0: 'Unknown',
+ 1: 'United States',
+ 2: 'State Government',
+ 3: 'Local Government',
+ 4: 'Private Individual',
+ 5: 'Business / Corporation',
+ 6: 'Organization / Union',
+ 7: 'Criminal Defendant',
+ 8: 'Other',
+ 9: 'Miscellaneous'}
